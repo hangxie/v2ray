335170a68756d20a47bdd3c0b620446685a11292
Hang Xie <7977860+hangxie@users.noreply.github.com>
2024-12-06 01:34:52 +0000

    setup multi platform build before buildx

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+docker context create multi-platform
+docker buildx create multi-platform --platform linux/amd64,linux/arm64,linux/arm --use
+
 VERSION=$(curl -s https://api.github.com/repos/v2fly/v2ray-core/releases/latest | jq -r .name)
 docker buildx build --push \
     --platform linux/amd64,linux/arm64,linux/arm \
